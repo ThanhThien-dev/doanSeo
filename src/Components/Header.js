77f3cea0644f90cd47e083887f2d55c9d26c31ac
Thanh Thiện <77567466+ThanhThien-dev@users.noreply.github.com>
2022-05-11 11:24:18 +0700

    Add files via upload

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 import { BsSearch } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 
 function Header() {
+  const [isMobile, setIsMobile] = useState(false);
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+    if (size > 1000) {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+
   return (
     <div className="header">
       <div className="left-header">
@@ -19,18 +38,27 @@ function Header() {
         ></input>
         <BsSearch className="search-button" size="30px"></BsSearch>
       </div>
-      <div className="right-header">
+      <div
+        className={isMobile ? "right-header-mobile" : "right-header"}
+        onClick={() => setIsMobile(false)}
+      >
         <Link to={"/tintuc"} className="non-textdecoration">
-          <div>Tin tức</div>
+          <div className="tintuc">Tin tức</div>
         </Link>
-        <div>Giỏ hàng</div>
-        <div>Trợ giúp</div>
-        <div>
+        <div className="giohang">Giỏ hàng</div>
+        <div className="trogiup">Trợ giúp</div>
+        <div className="lienhe">
           <a href="/" className="non-textdecoration">
             Liên hệ
           </a>
         </div>
       </div>
+      <button
+        className="mobile-menu-icon"
+        onClick={() => setIsMobile(!isMobile)}
+      >
+        {isMobile ? <FaTimes></FaTimes> : <FaBars></FaBars>}
+      </button>
     </div>
   );
 }
