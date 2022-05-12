8685d451e86bc2a95fcd35b77c3802c0ade390f8
phat <lvp3795@gmail.com>
2022-05-12 12:03:52 +0700

    ud2

--- src/Components/News.js
+++ src/Components/News.js
@@ -13,7 +13,7 @@ function News() {
               <div className="article-image">
                 <img src={require(`./images/${hinh}.png`)} alt="" />
               </div>
-              <Link to={`/tintuc/${id}`} state={{ idbaiviet: id }}>
+              <Link to={`/tintuc/${id}`}>
                 <div className="article-name">{tenbaiviet}</div>
               </Link>
               <div className="article-content">{content}</div>
